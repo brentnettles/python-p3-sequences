9ed9c9577e1a27eb3bfa548e08af346c3e4b416b
brentnettles <brentnettles@gmail.com>
2024-03-12 16:20:11 -0400

    completed lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+  f = []
+  if length == 0:
+    print([])
+  elif length == 1:
+    print([0])
+  elif length == 2:
+    print([0,1])
+  else:
+    f = [0,1]
+
+    for count in range(2, length):
+      next_count = f[-1] + f[-2]
+      f.append(next_count)
+    print(f)
+
+
+# print_fibonacci(10)
